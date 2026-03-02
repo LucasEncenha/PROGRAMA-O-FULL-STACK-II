6fd1659b49c0759aca48aace081cfdd4408af1cd
Lucas Encenha <lucasencenha@DESKTOP-OV9E62V.>
2026-03-02 20:50:35 -0300

    Criando rotas para movimentacoes de mercadorias atravez de notas fiscais

--- meu-backend/db.js
+++ meu-backend/db.js
@@ -1,10 +1,23 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config(); // Garante que carregou aqui também
+
+console.log("--- DEBUG DE CONEXÃO ---");
+console.log("Variável DB_PORT no .env:", process.env.DB_PORT);
 
 const pool = mysql.createPool({
-  host:     process.env.DB_HOST     || 'localhost',
-  user:     process.env.DB_USER     || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME     || 'controle_estoque'
+    host:     '127.0.0.1', 
+    port:     3307, // FORÇADO MANUALMENTE PARA O DOCKER
+    user:     'root',
+    password: 'root',
+    database: 'controle_estoque',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+pool.query('SHOW TABLES')
+  .then(([rows]) => {
+    console.log("📂 Tabelas que o Node consegue enxergar neste banco:", rows);
+  });
 
 module.exports = pool;
