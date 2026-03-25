09c5060a7d14283d23907411f158afd9ad6573c4
Lucas Encenha <lucasencenha@DESKTOP-OV9E62V.>
2026-03-24 23:38:43 -0300

    Desenvolvimento do frontend e integração ao backend

--- meu-frontend/src/pages/Home.jsx
+++ meu-frontend/src/pages/Home.jsx
@@ -6,10 +6,10 @@ export default function Home() {
             <h4 className="mb-4">Sistema de Controle de Estoque</h4>
             <div className="d-flex gap-3">
                 <Link to="/cadastro-nota" className="btn btn-outline-primary">
-                    📄 Notas Fiscais
+                    Notas Fiscais
                 </Link>
                 <Link to="/cadastro-produto" className="btn btn-outline-primary">
-                    📦 Produtos
+                    Produtos
                 </Link>
             </div>
         </div>
